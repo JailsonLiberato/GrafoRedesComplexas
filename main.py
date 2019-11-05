cefbdbae3ef51635a33b5728f29d2da43a254c06
jailsonliberato <jailsonliberato@gmail.com>
2019-11-05 01:59:09 -0300

    Table funcionando. Verificando gráficos.

--- main.py
+++ main.py
@@ -29,6 +29,10 @@ class Main:
 
     def execute(self):
         self.__plt_charts.generate_table_dataframe(self.__array_rede_small_world)
+        self.__plt_charts.generate_table_dataframe(self.__array_rede_erdos)
+        self.__plt_charts.generate_table_dataframe(self.__array_rede_barabasi_albert)
+        self.__plt_charts.generate_line_chart(self.__array_rede_small_world, self.__array_rede_erdos,
+                                              self.__array_rede_barabasi_albert)
         print("\n\n\n\t### Fim da aplicação...")
 
     def __execute_small_world(self, n, p):
--- plot_chart.py
+++ plot_chart.py
@@ -2,31 +2,55 @@ import matplotlib.pyplot as plt
 import numpy as np
 from rede import Rede
 import pandas as pd
-from pandas.table.plotting import table
+from pandas.plotting import table
+from constants import Constants
 
 
 class PlotChart:
     """Classe de geração de gráficos de análise das redes."""
 
-    def generate_bar_chart(self):
-        pass
+    def generate_line_chart(self, array1, array2, array3):
+        for x in range(len(Constants.N_ARRAY)):
+            array_rede1 = []
+            array_rede2 = []
+            array_rede3 = []
+            for y in range(len(Constants.P_ARRAY)):
+                rede1 = array1[(x*y) + y]
+                rede2 = array2[(x*y) + y]
+                rede3 = array3[(x*y) + y]
+                array_rede1.append(rede1.media)
+                array_rede2.append(rede2.media)
+                array_rede3.append(rede3.media)
+            plt.plot(array_rede1)
+            plt.plot(array_rede2)
+            plt.plot(array_rede3)
+            plt.legend(['Small Worlds', 'Erdos', 'Albert Barabasi'], loc='upper left')
+            plt.savefig(str(x + 1) + '.png')
+            plt.close()
 
-    def generate_table_dataframe(self, array:Rede):
+    def generate_table_dataframe(self, array: Rede):
+        """Gera a tabela o resumo dos dados."""
         medias = []
         medianas = []
         maxs = []
         mins = []
+        size: int = len(array)
         for rede in array:
             medias.append(rede.media)
             medianas.append(rede.mediana)
-            maxs.append(rede.max)
-            mins.append(rede.min)
-        data = [medias, medianas, maxs, mins] 
-        df = pd.DataFrame(data, columns = ['Média', 'Mediana', 'Max','Min'])
-        ax = plt.subplot(111, frame_on=False) # no visible frame
+            maxs.append(rede.maxima)
+            mins.append(rede.minima)
+        title = array[0].type
+        medias = np.array(medias).reshape(1, size).T
+        medianas = np.array(medianas).reshape(1, size).T
+        maxs = np.array(maxs).reshape(1, size).T
+        mins = np.array(mins).reshape(1, size).T
+        data = np.concatenate((medias, medianas, maxs, mins), axis=1)
+        df = pd.DataFrame(data, columns=['Média', 'Mediana', 'Max', 'Min'])
+        ax = plt.subplot(111, frame_on=False)  # no visible frame
+        ax.set_title(title, fontdict={'fontsize': 15, 'fontweight': 'medium'})
+        ax.table(cellText=df.values, colLabels=df.columns, loc='center')
         ax.xaxis.set_visible(False)  # hide the x axis
         ax.yaxis.set_visible(False)  # hide the y axis
-
-        table(ax, df)  # where df is your data frame
-
-        plt.savefig('mytable.png') 
+        plt.savefig('table summary' + title + '.png')
+        plt.close()
