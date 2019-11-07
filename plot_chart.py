1d62501c28ac2c6d2a689bdcb37d5916fa30f9da
jailsonliberato <jailsonliberato@gmail.com>
2019-11-06 22:12:19 -0300

    Gráficos barras funcionando.

--- plot_chart.py
+++ plot_chart.py
@@ -11,25 +11,25 @@ class PlotChart:
 
     def generate_bar_chart(self, small_world_array, erdos_array, albert_barabasi_array):
         for x in range(len(Constants.N_ARRAY)):
-            array_rede1 = [][]
-            array_rede2 = [][]
-            array_rede3 = [][]
+            array_rede1 = np.zeros((4, len(Constants.P_ARRAY)))
+            array_rede2 = np.zeros((4, len(Constants.P_ARRAY)))
+            array_rede3 = np.zeros((4, len(Constants.P_ARRAY)))
             for y in range(len(Constants.P_ARRAY)):
                 rede1 = small_world_array[(x*y) + y]
                 rede2 = erdos_array[(x*y) + y]
                 rede3 = albert_barabasi_array[(x*y) + y]
-                array_rede1.append(rede1.media, axis=0)
-                array_rede2.append(rede2.media, axis=0)
-                array_rede3.append(rede3.media, axis=0)
-                array_rede1.append(rede1.mediana, axis=1)
-                array_rede2.append(rede2.mediana, axis=1)
-                array_rede3.append(rede3.mediana, axis=1)
-                array_rede1.append(rede1.maxima, axis=2)
-                array_rede2.append(rede2.maxima, axis=2)
-                array_rede3.append(rede3.maxima, axis=2)
-                array_rede1.append(rede1.minima, axis=3)
-                array_rede2.append(rede2.minima, axis=3)
-                array_rede3.append(rede3.minima, axis=3)
+                array_rede1[0][y] = rede1.media
+                array_rede2[0][y] = rede2.media
+                array_rede3[0][y] = rede3.media
+                array_rede1[1][y] = rede1.mediana
+                array_rede2[1][y] = rede2.mediana
+                array_rede3[1][y] = rede3.mediana
+                array_rede1[2][y] = rede1.maxima
+                array_rede2[2][y] = rede2.maxima
+                array_rede3[2][y] = rede3.maxima
+                array_rede1[3][y] = rede1.minima
+                array_rede2[3][y] = rede2.minima
+                array_rede3[3][y] = rede3.minima
             self.__configure_bar_chart(array_rede1, array_rede2, array_rede3, str(Constants.N_ARRAY[x]))
     
 
@@ -38,16 +38,18 @@ class PlotChart:
 
         for i in range(4):
             # set height of bar
-            bars1 = small_world_array[i]
-            bars2 = erdos_array[i]
-            bars3 = albert_barabasi_array[i]
+            bars1 = small_world_array[i].tolist()
+            bars2 = erdos_array[i].tolist()
+            bars3 = albert_barabasi_array[i].tolist()
             
             # Set position of bar on X axis
             r1 = np.arange(len(bars1))
             r2 = [x + barWidth for x in r1]
             r3 = [x + barWidth for x in r2]
-            
             # Make the plot
+            title = self.__get_title_metric(i) + " " + n_name
+            ax = plt.subplot(111, frame_on=False)  # no visible frame
+            ax.set_title(title, fontdict={'fontsize': 15, 'fontweight': 'medium'})
             plt.bar(r1, bars1, color='r', width=barWidth, edgecolor='white', label='SmallWorlds')
             plt.bar(r2, bars2, color='b', width=barWidth, edgecolor='white', label='Erdos')
             plt.bar(r3, bars3, color='g', width=barWidth, edgecolor='white', label='Barabasi')
@@ -55,17 +57,11 @@ class PlotChart:
             # Add xticks on the middle of the group bars
             plt.xlabel('Probabilidades', fontweight='bold')
             plt.xticks([r + barWidth for r in range(len(bars1))], Constants.P_ARRAY)
-            plt.ylim(0, 5)
-            ax = plt.subplot(111, frame_on=False)  # no visible frame
-            title = self.__get_title_metric(i) + n_name
-            ax.set_title(title, fontdict={'fontsize': 15, 'fontweight': 'medium'})
             plt.legend()
-
-
-            plt.savefig(title + '.png')
+            plt.savefig('charts/' + title + '.png')
             plt.close()
 
-    def __get_title_metric(index_metric):
+    def __get_title_metric(self, index_metric):
         if(index_metric == 0):
             return "Media"
         elif(index_metric == 1):
